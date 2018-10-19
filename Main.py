2873ed9519f811b1dac02b3dcc4be17a626ce35a
Aurelien Thazet <thazet.aurelien@gmail.com>
2018-10-19 11:20:12 +0200

    Background and character test display

--- Main.py
+++ Main.py
@@ -7,10 +7,19 @@ pygame.init()
 
 #Création de la fenêtre
 window = pygame.display.set_mode((640, 480))
+#Affichage de l'image de test
+background = pygame.image.load("img/background_test.png").convert()
+window.blit(background, (0,0))
+#Affichage perso de test
+myCaracter = pygame.image.load("img/perso_test.png").convert_alpha()
+window.blit(myCaracter, (640/2, 480/2))
+#Rafraichissement de l'écran pour afficher les images
+pygame.display.flip()
 
 #Variable qui continue la boucle si = 1, stoppe si = 0
 continued = 1 
-
 #Boucle infinie permettant de faire tourner l'écran en continue
 while continued:
-    continue
+    for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+        if event.type == QUIT:     #Si un de ces événements est de type QUIT
+            continued = 0      #On arrête la boucle
