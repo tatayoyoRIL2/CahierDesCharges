a3374fccd25fffec188710c698ded2552659b950
Aurelien Thazet <thazet.aurelien@gmail.com>
2018-10-19 10:53:43 +0200

    V0.0.1 : Skeleton mini

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
-BLOP
+ #Importation des bibliothèques nécessaires
+import pygame
+from pygame.locals import *
+
+#Initialisation de la bibliothèque Pygame
+pygame.init()
+
+#Création de la fenêtre
+window = pygame.display.set_mode((640, 480))
+
+#Variable qui continue la boucle si = 1, stoppe si = 0
+continued = 1 
+
+#Boucle infinie permettant de faire tourner l'écran en continue
+while continued:
+    continue
