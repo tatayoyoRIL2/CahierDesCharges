b86f2cbf3e38f4e700b620b678bb41244be974fc
Romain <r.chevallier21@hotmail.fr>
2018-11-12 16:23:32 +0100

    can now click to put pawnsgit add resource/PionJaune.png (only on 1st row tho..)

--- example/RIL_puissance4_RcDevRIL_POC/puissance4.py
+++ example/RIL_puissance4_RcDevRIL_POC/puissance4.py
@@ -10,22 +10,66 @@ from pygame.locals import *
 #########VARIABLES GLOBALES#############
 
 origin = (0,0)
-screen = pygame.display.set_mode((1280,720),RESIZABLE)
+screen = pygame.display.set_mode((690,590),RESIZABLE)
+M = [[0, 0, 0, 0, 0, 0, 0], \
+     [0, 0, 0, 0, 0, 0, 0], \
+     [0, 0, 0, 0, 0, 0, 0], \
+     [0, 0, 0, 0, 0, 0, 0], \
+     [0, 0, 0, 0, 0, 0, 0], \
+     [0, 0, 0, 0, 0, 0, 0]]
+#me demandez pas comment j'ai trouver ces coordonnées...
+#ça veut dire que si on change de background, faudra refaire ça
 
+coord_M = [[(17,11), (114,11), (210,11), (307,11), (403,11), (500,11), (596,11)], \
+     [(17,109), (114,109), (210,109), (307,109), (403,109), (500,109), (596,109)], \
+     [(17,206), (114,206), (210,206), (307,206), (403,206), (500,206), (596,206)], \
+     [(17,304), (114,304), (210,304), (307,304), (403,304), (500,304), (596,304)], \
+     [(17,401), (114,401), (210,401), (307,401), (403,401), (500,401), (596,401)], \
+     [(17,498), (114,498), (210,498), (307,498), (403,498), (500,498), (596,498)]]
+
+
+joueur=1#joueur = 1 ou 2, pour choisir le bon pion et stocker les pions dans la matrice M
+draw=1
+i=0
+nbTours=0
+won=False
 ########################################
 
 pygame.init()#lancement de pygame
 
 ######CHARGEMENT DES IMAGES DU JEU######
 
-backGround=pygame.image.load("resource/Grille2.png").convert()
+backGround=pygame.image.load("./resource/Grille2.png").convert()
 sizeBG = backGround.get_size()
-yel = pygame.image.load("resource/PionJaune.png").convert_alpha() #convert_alpha() résoud la transparence
+yel = pygame.image.load("./resource/PionJaune.png").convert_alpha() #convert_alpha() résoud la transparence, normalement.........
 sizeYel = yel.get_size()
-red = pygame.image.load("resource/PionRouge.png").convert_alpha()
+red = pygame.image.load("./resource/PionRouge.png").convert_alpha()
 sizeRed = red.get_size()
 
 ########################################
+
+###########FONCTIONS################
+
+def choix_col(x,y):
+    return (int)((x-16)/97)
+
+def draw_pawns(pawns):
+    for i in range(len(pawns)):
+        for j in range(len(pawns[i])):
+            if pawns[i][j]==1:
+                screen.blit(yel,coord_M[i][j])
+                pygame.display.flip()
+            if pawns[i][j]==2:
+                screen.blit(red,coord_M[i][j])
+                pygame.display.flip()
+
+def yel_or_red(nb):
+    yel,red=1,2
+    if(nb%2==0):
+        return yel
+    else:
+        return red
+####################################
 
 #######AFFICHAGE "PLATEAU" DU JEU#######
 
@@ -33,12 +77,30 @@ screen = pygame.display.set_mode(sizeBG)
 screen.blit(backGround,origin)
 pygame.display.flip() #update TOUT le screen
 
-draw=1
-while draw:
+while (nbTours<42 and not won):
+
+    draw_pawns(M)
+    
+    
     for event in pygame.event.get():
 
         if event.type == QUIT:
-
-            draw = 0
-
+            won = True
+            
+        if event.type == MOUSEBUTTONUP:
+            if event.button == 1:#clic gauche
+                x,y = pygame.mouse.get_pos()
+                col = choix_col(x,y)
+                joueur = yel_or_red(nbTours)
+                M[5][col] = joueur
+                nbTours = nbTours+1
+                
+                
+#            if event.button == 3:#clic droit
+#                x_mouse=event.pos[0]
+#                y_mouse=event.pos[1]
+#                
+#                screen.blit(yel,coord_M[i][0])
+#                i=i+1
+                
 screen = pygame.display.quit()
